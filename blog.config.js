53e925d02daa9f7d738f1c8b725046f62614e70e
ThrowFeeder <dsnwsnbb@gmail.com>
2023-12-15 16:33:23 +0800

    upgrade config

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'what man, 只会截胡外卖员的保安, ni is mo fa pi fengs, 立志当上清华保安队队长🎉',
 
-  CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
+  CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'T7eeder', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '赛博保安保安👨‍✈️', // 作者简介
